464cfae8722c13f39b3b6d72a1631b287e1e4d22
milindvb <mibelhe@microsoft.com>
2021-03-11 15:28:59 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,8 +4,7 @@ apt install brotli
 a2enmod brotli
 
 echo "<IfModule mod_brotli.c>" >> /etc/apache2/apache2.conf
-echo "AddOutputFilterByType BROTLI_COMPRESS text/html text/plain text/xml text/css text/javascript application/x-javascript application/javascript application/json application/x-font-ttf application/vnd.ms-fontobject image/x-icon
-" >> /etc/apache2/apache2.conf
+echo "AddOutputFilterByType BROTLI_COMPRESS text/html text/plain text/xml text/css text/javascript application/x-javascript application/javascript application/json application/x-font-ttf application/vnd.ms-fontobject image/x-icon" >> /etc/apache2/apache2.conf
 echo "<\/IfModule>" >> /etc/apache2/apache2.conf
 
 /usr/sbin/apache2ctl -D FOREGROUND
